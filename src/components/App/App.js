37f92a05567f9695b01f7b4351c49ff4dfd03145
Jane Yushchenko <kelenella@ukr.net>
2021-11-26 20:33:39 +0200

    initial files

---
+++ src/components/App/App.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import Container from '../Container';
+import ContactForm from '../ContactForm';
+import Filter from '../Filter';
+import ContactList from '../ContactsList';
+import initialContacts from '../../contacts.json';
+import s from './App.module.css';
+
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem('contacts')) ?? initialContacts
+    );
+  });
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = data => {
+    const comparableElement = contacts.some(
+      element => element.name.toLowerCase() === data.name.toLowerCase(),
+    );
+    if (comparableElement) {
+      return alert('contact is already in the directory');
+    }
+
+    setContacts([...contacts, data]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const getVisibleContact = () => {
+    const contactFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(contactFilter),
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <Container>
+      <h1 className={s.title}>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <Filter value={filter} onChange={changeFilter} />
+      <h2 className={s.contactTitle}>Contacts</h2>
+      <ContactList
+        contacts={getVisibleContact()}
+        onDeleteContact={deleteContact}
+      />
+    </Container>
+  );
+}
+
+export default App;

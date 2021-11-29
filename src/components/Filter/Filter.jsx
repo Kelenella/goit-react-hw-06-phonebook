5ce7458f492c3514cb750a46b4836d1f86b8cf56
Jane Yushchenko <kelenella@ukr.net>
2021-11-29 11:36:14 +0200

    updated styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,10 +4,10 @@ import s from './Filter.module.css';
 export default function Filter({ value, onChange }) {
   return (
     <div className={s.filter}>
-      <label className={s.text}>
+      <label className={s.label}>
         Find contacts by name
         <input
-          className={s.value}
+          className={s.input}
           type="text"
           value={value}
           onChange={onChange}

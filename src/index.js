79cb2306d183c414ca88d8c5be1e1fe1f1768aa8
Jane Yushchenko <kelenella@ukr.net>
2021-11-30 23:14:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './components/App';
 //импорт компонента Провайдер
 import { Provider } from 'react-redux';
 //импорт объекта глобального состояния
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<h2>Wait,loading</h2>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
